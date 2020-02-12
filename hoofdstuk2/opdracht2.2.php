c7e7ef1cfd87fc8b0fdefa98c98aacc7a82c5293
Rosalie Bloemers <rosalie.bloemers@edu-kw1c.nl>
2020-02-12 12:00:47 +0100

    ik het opdracht2.2.php af

--- hoofdstuk2/opdracht2.2.php
+++ hoofdstuk2/opdracht2.2.php
@@ -15,6 +15,36 @@
     <link href="" rel="stylesheet" type="text/css">
 
 </head>
+<header>
+    <h1 class="title">
+        <?php echo "Uitwerking van PHP-opdrachten"; ?>
+    </h1>
+</header>
+<body>
 <?php
+$txt1 = "Hallo";
+$txt2 = "een makkelijke taal";
+$txt3 = "toch zo'n makkelijke taal";
+$txt4 = "wat is";
+$txt5 = "PHP";
+$txt6 = "Nooit gedacht dat";
+$txt7 = "De installatie is best ingewikkeld";
+$txt8 = "Fijn";
+$txt9 = "?";
+$txt10 = ".";
+$txt11 = ",";
+$txt12 = "<br>";
+$txt13 = "is";
+$txt14 = "Vind je niet";
+$txt15 = "toch";
 
+echo "<p> 
+$txt1$txt11 $txt4 $txt3$txt10$txt12
+$txt7$txt10 $txt8 $txt15$txt9$txt12
+$txt6 $txt3 $txt13$txt10
+ </p>"
 ?>
+<footer>
+    <a href="../index.php" class="back">klik hier om terug te gaan</a>
+</footer>
+</body>
