d3268b903a67cc34f24e5a491ef427d921509c17
Rosalie Bloemers <rosalie.bloemers@edu-kw1c.nl>
2020-03-23 15:03:24 +0100

    aangepast script.php

--- hoofdstuk4/script.php
+++ hoofdstuk4/script.php
@@ -9,52 +9,52 @@ $teacherName ="";
 
 switch ($courseName) {
 
-case "PHP":
-$teacherName = "Abu";
-break;
+    case "PHP":
+    $teacherName = "Abu";
+    break;
 
-case "javascript":
-$teacherName = "Evers";
-break;
+    case "javascript":
+    $teacherName = "Evers";
+    break;
 
-case "rekenen":
-$teacherName = "Wetering";
-break;
+    case "rekenen":
+    $teacherName = "Wetering";
+    break;
 
-case "nederlands":
-$teacherName = "van den Ende";
-break;
+    case "nederlands":
+    $teacherName = "van den Ende";
+    break;
 
-case "engels":
-$teacherName = "Mitrovic-Vejzovic";
-break;
+    case "engels":
+    $teacherName = "Mitrovic-Vejzovic";
+    break;
 
-case "modelleren":
-$teacherName = "van Bijnen";
-break;
+    case "modelleren":
+    $teacherName = "van Bijnen";
+    break;
 
-case "ASP":
-$teacherName = "Gijsbrechts";
-break;
+    case "ASP":
+    $teacherName = "Gijsbrechts";
+    break;
 
-case "HTML/CSS":
-$teacherName = "Wetering";
-break;
+    case "HTML/CSS":
+    $teacherName = "Wetering";
+    break;
 
-case "computertekenen":
-$teacherName = "van den Berg";
-break;
+    case "computertekenen":
+    $teacherName = "van den Berg";
+    break;
 
-case "LB":
-$teacherName = "Visser";
-break;
+    case "LB":
+    $teacherName = "Visser";
+    break;
 
-case "SLB":
-$teacherName = "Visser";
-break;
+    case "SLB":
+    $teacherName = "Visser";
+    break;
 
-case "digitalevaardigheden":
-$teacherName = "Pielage";
-break;
-}
-?>
+    case "digitalevaardigheden":
+    $teacherName = "Pielage";
+    break;
+    }
+    ?>
