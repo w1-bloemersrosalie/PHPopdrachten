43057b5319e4fb44caee614d31b160f4ff1b48c8
Rosalie Bloemers <rosalie.bloemers@edu-kw1c.nl>
2020-03-23 14:07:16 +0100

    aangepast

--- hoofdstuk4/script4.3.php
+++ hoofdstuk4/script4.3.php
@@ -5,21 +5,32 @@
 
 
 <?php
-$task2 = 'lancering in';
+$task2 = 'lancering in:';
 $number = 19;
 
 
-while($number >= 1)
+while($number >= 0)
 {
-   print "$number,";
+    $task2 =$task2.$number;
+    if ($number > 0)
+    {
+        $task2 = $task2. ",";
+    }
     $number--;
 }
+print $task2
 ?>
 <br>
 <?php
-$task3 = "lancering in";
-for($number2 = 19; $number2 >= 1; $number2--)
+$task3 = "lancering in:";
+for($number2 = 19; $number2 >= 0; $number2--)
 {
-    print "$number2,";
+    $task3 = $task3.$number2;
+    if ($number2 > 0)
+    {
+        $task3 = $task3. ",";
+    }
+
 }
+print $task3;
 ?>
