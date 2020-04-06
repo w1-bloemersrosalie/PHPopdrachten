33a5c82ebdafb59afce72b10bf8215932617f40a
Rosalie Bloemers <rosalie.bloemers@edu-kw1c.nl>
2020-04-06 13:02:18 +0200

    nieuwe opdracht 5.2

---
+++ hoofdstuk5/form_data5.2.php
@@ -0,0 +1,18 @@
+<?php /**
+ * User: Rosalie Bloemers
+ * Date: 6-4-2020
+ */ ?>
+
+<h1>Uitschrijfformulier KW1C</h1>
+<hr>
+<?php
+echo "<table class='eenvoudig'>";
+echo "<tr><td>voor en achternaam</td><td>".$_GET["name"]."</td></tr>";
+echo "<tr><td>studentnummer</td><td>".$_GET["studentnumber"]."</td></tr>";
+echo "<tr><td>datum van uitschrijving</td><td>".$_GET["date"]."</td></tr>";
+echo "<tr><td>reden van uitschrijving</td><td>".$_GET["reasons"]."</td></tr>";
+echo "<tr><td>leerjaar</td><td>".$_GET["class"]."</td></tr>";
+echo "<tr><td></td><td>".$_GET["reasons2"]."</td></tr>";
+echo "<tr><td>de reden van uitschrijving</td><td>".$_GET["message"]."</td></tr>";
+echo "</table>";
+?>
--- hoofdstuk5/opdracht5.2.php
+++ hoofdstuk5/opdracht5.2.php
@@ -1,11 +1,88 @@
 <?php /**
  * User: Rosalie Bloemers
- * Date: 25-3-2020
+ * Date: 6-4-2020
  */ ?>
 
 <?php include '../header.php'; ?>
 <body>
-
+<h1>Uitschrijfformulier KW1C</h1>
+<hr>
+<form action="form_data5.2.php" method="get">
+    <table>
+        <tr>
+            <td>
+                <label>Voor en achternaam: </label>
+            </td>
+            <td>
+                <input name="name" type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Studentennummer</label>
+            </td>
+            <td>
+                <input name="studentnumber" type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Datum van uitschrijving</label>
+            </td>
+            <td>
+                <input name="date" type="date">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Reden van uitschrijving</label>
+            </td>
+            <td>
+                <select name="reasons">
+                    <option value="verkeerde keuze">verkeerde keuze</option>
+                    <option value="blijven zitten">blijven zitten</option>
+                    <option value="anders">anders</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Leerjaar</label>
+            </td>
+            <td>
+                <br>
+                <input type="radio" name="class" value="1ste leerjaar"> 1ste leerjaar<br>
+                <input type="radio" name="class" value="2de leerjaar"> 2de leerjaar<br>
+                <input type="radio" name="class" value="3de leerjaar"> 3de leerjaar
+            </td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td>
+                <input type="checkbox"  name="reasons2" value="Ik wil me aanmelden bij de succesklas">
+                <label for="succes">Ik wil me aanmelden bij de succesklas </label><br>
+                <input type="checkbox"  name="reasons2" value="Verwijder mijn gegevens uit het systeem">
+                <label for="delete">Verwijder mijn gegevens uit het systeem</label><br>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Geef hieronder de reden van je uitschrijving op</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input name="message" type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" name="verzend" value="Versturen">
+            </td>
+        </tr>
+    </table>
+</form>
 
 </body>
 <?php include '../footer.php'; ?>
